a87520c374c37fe1bbd6983ab9935e91440a7b18
mcicishvilii <mcicishvilii@gmail.com>
2024-05-12 15:27:58 +0400

    doing something

--- ui/components/settingscreen.js
+++ ui/components/settingscreen.js
@@ -8,14 +8,70 @@ import {
   View,
   SafeAreaView,
 } from "react-native";
+import React, { useEffect, useState } from "react";
 
-export default function SettingsScreen() {
-  // ... Your settings screen content
+
+
+const itemsPerPage = 16;
+
+const SettingsScreen = ({ navigation }) => {
+  const [totalPages, setTotalpages] = useState(0);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [items, setItems] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
+
+  useEffect(() => {
+    fetchData();
+  }, [currentPage]);
+  
+  async function fetchData() {
+    setRefreshing(true);
+    try {
+      let response = await fetcher(currentPage, itemsPerPage);
+      setTotalpages(response.metadata.total / itemsPerPage);
+      let data: [] = response.results;
+      setItems(data);
+      setRefreshing(false);
+    } catch (error) {
+      setRefreshing(false);
+      console.log(error);
+    }
+  }
+
   return (
-    <View style={{ flex: 1, backgroundColor: "red" , justifyContent: "space-between"}}>
-      <View style={{ flex: 2, backgroundColor: "gold" }}></View>
-      <View style={{ flex: 4, backgroundColor: "dodgerblue" }}></View>
-      <View style={{ flex: 16, backgroundColor: "tomato" }}></View>
+    <View style={styles.container}>
+      <View style={styles.buttonContainer}>
+        <Button title="Refresh Data" onPress={() => navigation.navigate("NotesScreen")} />
+      </View>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  contentContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  insideCont: {
+    padding: 10,
+    backgroundColor: "green",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    marginBottom: 20,
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+});
+
+export default SettingsScreen;
